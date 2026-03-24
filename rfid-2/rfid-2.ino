bcb5bf264b6535e019008b2b9a78838d71dc122e
Hanyeol Ryu <johndoe5576@gmail.com>
2026-03-24 15:53:09 +0900

    Add files via upload

--- rfid-2/rfid-2.ino
+++ rfid-2/rfid-2.ino
@@ -1,1 +1,68 @@
+#include <SoftwareSerial.h>
+#include <AFMotor.h>
+#include "SPI.h"
+#include "MFRC522.h"
+#include <DFRobotDFPlayerMini.h>
+#include "VoiceRecognitionV3.h"
 
+// RFID part
+// SDA 53 | SCK 52 | MOSI 51 | MISO 50 | RST 49
+#define SDA_PIN 53
+#define RST_PIN 49
+MFRC522 mfrc522(SDA_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+
+int isKey() {
+  if (mfrc522.PICC_IsNewCardPresent() & mfrc522.PICC_ReadCardSerial()) {
+
+    MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K) 
+      {
+      Serial.println(F("Your tag is not of type MIFARE Classic."));
+      return;
+      }
+    String rfid = "";
+    for (byte i = 0; i < 4; i++) {
+      rfid +=
+      (mfrc522.uid.uidByte[i] < 0x10 ? "0" : "") +
+      String(mfrc522.uid.uidByte[i], HEX) +
+      (i!=3 ? ":" : "");
+    }
+  
+    Serial.print("Card key: ");
+    Serial.println(rfid);
+
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
+    if (rfid == "c0:8f:9e:25") {
+      Serial.println("START");
+    }
+    if (rfid == "e8:ea:cf:0d") {
+      Serial.println("OBJ_1");
+    }
+    if (rfid == "e8:a0:6f:0d") {
+      Serial.println("OBJ_2");
+    }
+    if (rfid == "fb:df:c7:22") {
+      Serial.println("OBJ_3");
+    }
+    else {
+      return -1;
+    }
+    //delay(1000);
+  }
+  return -1;
+}
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
+
+void loop() {
+  isKey();
+
+}
