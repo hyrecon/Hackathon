ac9b552d358663a3f4a5fb8751fd068484dfd6cc
Hanyeol Ryu <johndoe5576@gmail.com>
2026-03-24 15:50:49 +0900

    Add files via upload

--- MIC/MIC.ino
+++ MIC/MIC.ino
@@ -1,1 +1,22 @@
+#include <SoftwareSerial.h>
+#include "VoiceRecognitionV3.h"
+VR myVR(2,3);    // 2:RX 3:TX,
+uint8_t records[7];
+uint8_t buf[64];
 
+void setup()
+{
+  myVR.begin(9600);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  int ret;
+  ret = myVR.recognize(buf, 50);
+  if(ret>0)
+  {
+    Serial.print(buf[1]);
+    Serial.println("");
+  }
+}
